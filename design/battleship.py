ffd83bd6638b4213ce12cb92c1e91cda885e462d
Baso1st <abdulwanis1@gmail.com>
2022-07-16 22:07:27 -0400

    Refactored to do some polymorphism and encapsulation.

--- design/battleship.py
+++ design/battleship.py
@@ -1,8 +1,8 @@
+from abc import ABC
 
 class Direction:
     Vertical = 1
     HORIZONTAL = 2
-
 
 class ShipPlacement:
     def __init__(self, i, j, direction):
@@ -10,119 +10,104 @@ class ShipPlacement:
         self.j = j
         self.direction = direction
 
-
-class Ship:
-    def __init__(self, size, shipPlacement):
-        #self.name = name
+class Ship(ABC):
+    def __init__(self, name, size):
+        self.name = name
         self._size = size
-        self._status = 1
-        self._build_coordinates(size, shipPlacement)
-    
-    def _build_coordinates(self, size, shipPlacement):
-        coords = []
-        for idx in range(size):
-            if shipPlacement.direction == Direction.Vertical:
-                coords.append((shipPlacement.i, shipPlacement.j + idx))
-            else:
-                coords.append((shipPlacement.i + idx, shipPlacement.j))
-        self.coordinates = coords
-
 
     @property
-    def coordinates(self):
-        return self._coordinates
+    def size(self):
+        return self._size
 
-    @coordinates.setter
-    def coordinates(self, coords):
-        self._coordinates = coords
-
-    def take_hit(self, i, j):
-        self.coordinates.remove((i, j))
+    def take_hit(self):
         self._size -= 1
-        if self._size == 0:
-            self._status = 0
-    
-    def is_destroyed(self):
-        return self._status == 0
+        return self.get_hit_report()
 
-    def get_public_info(self):
-        return f"Ship Type: {type(self).__name__} is criticaly hit. Remaining health: {self._size}"
+    def get_hit_report(self):
+        message = f"{type(self).__name__} {self.name} "
+        if self._size > 0:
+            return message + f"is criticaly hit. Remaining health: {self._size}"
+        else:
+            return message + f"has been destroyed"
 
-# class AirCraftCarrier(Ship):
-#     def __init__(self, name):
-#         super().__init__(name)
-#         self.size = 5
+class AirCraftCarrier(Ship):
+    def __init__(self, name):
+        super().__init__(name, 5)
 
+class Destroyer(Ship):
+    def __init__(self, name):
+        super().__init__(name, 3)
 
+class SmallBoat(Ship):
+    def __init__(self, name):
+        super().__init__(name, 1)
+
+class Submarine(Ship):
+    def __init__(self, name):
+        super().__init__(name, 2)
 
 class BattleShipGame:
     def __init__(self):
-        self.player_one_ships = []
-        self.player_two_ships = []
+        self.boards = [None, {}, {}]
         self.winner = 0
 
+    def place_ship(self, player, ship, shipPlacement):
+        coords = self._get_coords(ship.size, shipPlacement)
+        board = self.boards[player]
+        for coord in coords: 
+            board[coord] = ship
 
-    def place_ship(self, player, ship):
-        if player == 1:
-            self.player_one_ships.append(ship)
+    def _get_coords(self, size, shipPlacement):
+        coords = []
+        for idx in range(size):
+            if shipPlacement.direction == Direction.Vertical:
+                coords.append((shipPlacement.i + idx, shipPlacement.j))
+            else:
+                coords.append((shipPlacement.i, shipPlacement.j + idx))
+        return coords
+
+    def hit(self, coord, player):
+        #Basic input validation.
+        other_player = 2 if player == 1 else 1
+        board = self.boards[other_player]
+        if coord in board:
+            ship = board.pop((i, j))
+            hit_effect = ship.take_hit()
+            if not board:
+                self.winner = player
+            return hit_effect
         else:
-            self.player_two_ships.append(ship)
-
-
-    def hit(self, i, j, player): # Refactor for performance
-        ships = self.player_one_ships if player == 2 else self.player_two_ships
-        hit_ship = None
-        for ship in ships:
-            if (i, j) in ship.coordinates:
-                hit_ship = ship
-                hit_ship.take_hit(i, j)
-                break
-
-        if hit_ship:
-            if hit_ship.is_destroyed():
-                ships.remove(hit_ship)
-
-            if not ships:
-                self.winner == player
-
-            return hit_ship.get_public_info()
-        else:
-            return f"You missed!"
+            return "You missed! Big splash of water"
 
 
 if __name__== '__main__':
-    # 1 AirCraftCarrier size  5
-    # 2 destroyers size  3
-    # 4 small boat size  1
-    # 3 Submarines size  2
-
     game = BattleShipGame()
 
-    print(f"Player 1: Please send your ship locations, don't worry this information will be a secret ;)")
+    print("Seting up player 1 ships")
 
-    game.place_ship(1, Ship(5, ShipPlacement(0, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(3, ShipPlacement(1, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(3, ShipPlacement(2, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(1, ShipPlacement(3, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(1, ShipPlacement(4, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(1, ShipPlacement(5, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(1, ShipPlacement(6, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(2, ShipPlacement(7, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(2, ShipPlacement(8, 0, Direction.HORIZONTAL)))
-    game.place_ship(1, Ship(2, ShipPlacement(9, 0, Direction.HORIZONTAL)))
+    game.place_ship(1, AirCraftCarrier("USS Thunder"), ShipPlacement(0, 0, Direction.HORIZONTAL))
+    game.place_ship(1, Destroyer("USS Navy Pride"), ShipPlacement(1, 0, Direction.HORIZONTAL))
+    game.place_ship(1, Destroyer("USS Navigator"), ShipPlacement(2, 0, Direction.HORIZONTAL))
+    game.place_ship(1, SmallBoat("USS boat1"), ShipPlacement(3, 0, Direction.HORIZONTAL))
+    game.place_ship(1, SmallBoat("USS boat2"), ShipPlacement(4, 0, Direction.HORIZONTAL))
+    game.place_ship(1, SmallBoat("USS boat3"), ShipPlacement(5, 0, Direction.HORIZONTAL))
+    game.place_ship(1, SmallBoat("USS boat4"), ShipPlacement(6, 0, Direction.HORIZONTAL))
+    game.place_ship(1, Submarine("USS Silent Hunter"), ShipPlacement(7, 0, Direction.HORIZONTAL))
+    game.place_ship(1, Submarine("USS Whisperer"), ShipPlacement(8, 0, Direction.HORIZONTAL))
+    game.place_ship(1, Submarine("USS Sea snake"), ShipPlacement(9, 0, Direction.HORIZONTAL))
 
-    print(f"Player 2: Please send your ship locations, don't worry this information will be a secret ;)")
+    print("Seting up player 2 ships")
 
-    game.place_ship(2, Ship(5, ShipPlacement(0, 0, Direction.Vertical)))
-    game.place_ship(2, Ship(3, ShipPlacement(0, 1, Direction.Vertical)))
-    game.place_ship(2, Ship(3, ShipPlacement(0, 2, Direction.Vertical)))
-    game.place_ship(2, Ship(1, ShipPlacement(0, 3, Direction.Vertical)))
-    game.place_ship(2, Ship(1, ShipPlacement(0, 4, Direction.Vertical)))
-    game.place_ship(2, Ship(1, ShipPlacement(0, 5, Direction.Vertical)))
-    game.place_ship(2, Ship(1, ShipPlacement(0, 6, Direction.Vertical)))
-    game.place_ship(2, Ship(2, ShipPlacement(0, 7, Direction.Vertical)))
-    game.place_ship(2, Ship(2, ShipPlacement(0, 8, Direction.Vertical)))
-    game.place_ship(2, Ship(2, ShipPlacement(0, 9, Direction.Vertical)))
+    game.place_ship(2, AirCraftCarrier("Alien Thunder"), ShipPlacement(0, 0, Direction.Vertical))
+    game.place_ship(2, Destroyer("Alien Navy Pride"), ShipPlacement(1, 0, Direction.Vertical))
+    game.place_ship(2, Destroyer("Alien Navigator"), ShipPlacement(2, 0, Direction.Vertical))
+    game.place_ship(2, SmallBoat("Alien boat1"), ShipPlacement(3, 0, Direction.Vertical))
+    game.place_ship(2, SmallBoat("Alien boat2"), ShipPlacement(4, 0, Direction.Vertical))
+    game.place_ship(2, SmallBoat("Alien boat3"), ShipPlacement(5, 0, Direction.Vertical))
+    game.place_ship(2, SmallBoat("Alien boat4"), ShipPlacement(6, 0, Direction.Vertical))
+    game.place_ship(2, Submarine("Alien Silent Hunter"), ShipPlacement(7, 0, Direction.Vertical))
+    game.place_ship(2, Submarine("Alien Whisperer"), ShipPlacement(8, 0, Direction.Vertical))
+    game.place_ship(2, Submarine("Alien Sea snake"), ShipPlacement(9, 0, Direction.Vertical))
 
     print('Thank you for placing your ships, Beign the battle! May the odds be in your favor')
 
@@ -136,7 +121,7 @@ if __name__== '__main__':
         
         (i, j) = [int(x) for x in input(f"Player {player} where do you hit?").split()]
 
-        print(game.hit(i, j, player))
+        print(game.hit((i, j), player))
 
         round += 1
     
